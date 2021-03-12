a24d0629ad51f10102473f49b765f385b5083b90
ico1036 <jiwoong.kim@cern.ch>
2021-03-12 17:06:52 +0900

    ak1 + ak0

--- Uproot/beta.py
+++ Uproot/beta.py
@@ -54,40 +54,40 @@ MET = ak.zip({
 })
 
 
-print("Ele: ",Electron,len(Electron))
-print("Pho: ",Photon,len(Photon))
-print("MET: ",MET,len(MET))
 
 ## -- Event Selection: at least 1 Electron -- ##
 
 Electron_evt_mask = ak.num(Electron.PT) > 0
 Evt_mask = Electron_evt_mask
 
-Electron	=  Electron[Evt_mask]
-MET	        =  MET[Evt_mask]
+Electron_sel	=  Electron[Evt_mask]
+MET_sel	        =  MET[Evt_mask]
 print("Selected Events: ",len(Electron))
 
 
-## -- <Use Ak0 method>
+## W --MT calculation     <Use Ak0 donwn-grade ...  ak1 with vector method is on developing..>
+# ! -------Warning ------>  ak.zip modify the an array type ak0-based jagged-array to ak1-based High level-array
+# You should condiser this when you try the ak0-based method eg. uproot method
+
 def Make_T2Vector(pt,phi):
 	pt  = ak.to_awkward0(pt)
 	phi = ak.to_awkward0(phi)
 	return TVector2Array.from_polar(pt,phi)
 
 
+Electron_T2vec = Make_T2Vector(Electron_sel.PT,Electron_sel.Phi)
+MET_T2vec	   = Make_T2Vector(MET_sel.MET,MET_sel.Phi)
 
-## -- Leading Electron physics variables -- ##
-Electron_T2vec = Make_T2Vector(Electron.PT,Electron.Phi)
-MET_T2vec	   = Make_T2Vector(MET.MET,MET.Phi)
+Leading_Electron		   = Electron_sel[ak.argmax(Electron_sel.PT,axis=1,keepdims=True)]
+Leading_Electron_T2Vec	   = Make_T2Vector(Leading_Electron.PT,Leading_Electron.Phi)
 
-Leading_Electron		   = Electron[ak.argmax(Electron.PT,axis=1,keepdims=True)]
-Leading_Electron_T2Vec	   = Electron_T2vec[ak.argmax(Electron.PT,axis=1)] 
+# !--------- Warning ---------! 
+#  ak0-based jagged array has shape of (N,) but ak1-based high level array has shape of (N,N)
+#  Do not match this dimension with ak.flatten() or array.sum() 
+#  This difference is from the different version
+#  Therefore, please use the ak.to_awkward0 method to match the difference dimensions from difference ak version
+MT_e = np.sqrt(2*ak.to_awkward0(Leading_Electron.PT) *ak.to_awkward0(MET_sel.MET)*(1-np.cos(MET_T2vec.delta_phi(Leading_Electron_T2Vec))))
 
-
-## -- <Use Ak0 method>
-MT_e = np.sqrt(2*Leading_Electron.PT * MET.MET)*(1-np.cos(MET_T2vec.delta_phi(Leading_Electron_T2Vec)))
-
-print("W MT: ", MT_e)
 
 
 ## -- Output hist & Ntuple(will be added)  -- ##
@@ -98,7 +98,6 @@ print("W MT: ", MT_e)
 
 histo = {}
 histo['MT'] = ak.flatten(MT_e)
-
 
 outname = sample_names[0] + ".npy" # Just one file
 
