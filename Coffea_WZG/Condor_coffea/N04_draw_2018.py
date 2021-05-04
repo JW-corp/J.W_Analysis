9256f585a2be3d7c4091ef3064e75dce8a03f057
ico1036 <jiwoong.kim@cern.ch>
2021-05-04 10:18:08 +0900

    Baseline check

--- Coffea_WZG/Condor_coffea/N04_draw_2018.py
+++ Coffea_WZG/Condor_coffea/N04_draw_2018.py
@@ -59,7 +59,7 @@ hsum_dR_aj = hist.Hist(
 hsum_cutflow = hist.Hist(
 	'Events',
 	hist.Cat('dataset', 'Dataset'),
-	hist.Bin('cutflow', 'Cut index', [0, 1, 2, 3,4,5,6,7])
+	hist.Bin('cutflow', 'Cut index', [0, 1, 2, 3,4,5,6,7,8])
 )
 
 hsum_charge= hist.Hist(
@@ -266,18 +266,13 @@ def reduce(folder,sample_list,histname):
 
 
 ## --File Directories
-file_path = "Base_line_210416_Allweight"
-#file_path = "Base_line_210416_no_weight"
-#file_path = "SR_210419"
-#file_path = "CR_Zjets_210419"
-#file_path = "CR_tenriched_210419"
-#file_path = "CR_conversion_210419"
-#file_path = "210420_SEN_BaseLine"
+
+file_name = "210504_Baseline"
+file_path = "results/" + file_name
 
 
 ## --Sample Lists
 sample_list = ['DY' ,'WZ' ,'ZZ' ,'TTWJets','TTZtoLL','tZq' ,'Egamma','WZG','ZGToLLG','TTGJets','WGToLNuG']
-
 
 
 
@@ -305,7 +300,7 @@ sample_list = ['DY' ,'WZ' ,'ZZ' ,'TTWJets','TTZtoLL','tZq' ,'Egamma','WZG','ZGTo
 #histname = "ele1phi"; xmin=-3.15; xmax=3.15; ymin=100; ymax=5e+6;
 #histname = "ele2phi"; xmin=-3.15; xmax=3.15; ymin=100; ymax=5e+6;
 
-#histname = "cutflow"; xmin=0; xmax=6; ymin=1; ymax=5e+6
+histname = "cutflow"; xmin=0; xmax=6; ymin=1; ymax=5e+6
 
 		# --- Photon --- #
 
@@ -318,7 +313,7 @@ sample_list = ['DY' ,'WZ' ,'ZZ' ,'TTWJets','TTZtoLL','tZq' ,'Egamma','WZG','ZGTo
 #histname = "pho_EB_Iso_chg"; xmin=0; xmax=0.03; ymin=0.01; ymax=5e+7;
 
 #histname = "dR_aj"; xmin=0; xmax=1; ymin=0.01; ymax=10;
-histname = "pho_EE_pt"; xmin=0; xmax=200; ymin=0; ymax=8;
+#histname = "pho_EE_pt"; xmin=0; xmax=200; ymin=0; ymax=8;
 #histname = "pho_EE_eta"; xmin=-3; xmax=3; ymin=1; ymax=5e+6;
 #histname = "pho_EE_phi"; xmin=-3.15; xmax=3.15; ymin=1; ymax=5e+6;
 #histname = "pho_EE_hoe"; xmin=0; xmax=0.2; ymin=0.001; ymax=5e+6;
@@ -332,8 +327,6 @@ histname = "pho_EE_pt"; xmin=0; xmax=200; ymin=0; ymax=8;
 ## --All-reduce 
 h1,sumwdict = reduce(file_path,sample_list,histname)
 
-for i,j in sumwdict.items():
-	print(i,": ",j)
 
 ## --Noramlize	
 scales={
@@ -363,7 +356,7 @@ h1.scale(scales,axis='dataset')
 #h1 = h1.rebin(histname,hist.Bin("mass","M_{ee} [GeV]",50,0,200))
 
 #h1 = h1.rebin(histname,hist.Bin("pho_EB_pt","Photon EB $P_{T}$ [GeV]", 30, 0, 600))
-h1 = h1.rebin(histname,hist.Bin("pho_EE_pt","Photon EE $P_{T}$ [GeV]", 30, 0, 600))
+#h1 = h1.rebin(histname,hist.Bin("pho_EE_pt","Photon EE $P_{T}$ [GeV]", 30, 0, 600))
 
 
 
@@ -427,9 +420,17 @@ data_err_opts = {
 }
 
 
+# -- Make order of stak
 print("##" * 20)
-for i,j in h1.values().items():
-	print(i,":",j[-2])
+order_dict = {}
+for i, j in h1.values().items():
+	order_dict[i[0]] = j[-2]
+
+orderd_dict = dict(sorted(order_dict.items(),key=(lambda x: x[1])))
+for i,j in orderd_dict.items():
+	print("{0} : {1}".format(i,j))
+
+
 
 
 # MC plotting
@@ -441,7 +442,7 @@ hist.plot1d(
 	ax=ax,
 	clear=False,
 	stack=True,
-	order=['TTWJets' ,'tZq' ,'TTGJets' ,'TTZtoLL' ,'WZG' ,'WZ' ,'ZZ' ,'ZGToLLG'],
+	order=['TTWJets' ,'tZq' ,'TTGJets' ,'TTZtoLL','WZ' ,'ZZ' ,'ZGToLLG','WZG'],
 	fill_opts=fill_opts,
 	error_opts = error_opts,
 )
@@ -456,50 +457,47 @@ hist.plot1d(
 	error_opts=data_err_opts
 	
 )
-
-#print(h1['Egamma'].values())
-
-# Ratio Plot
+# -- Ratio Plot
 hist.plotratio(
-	num=h1['Egamma'].sum("dataset"),
-	denom=h1[notdata].sum("dataset"),
-	ax=rax,
-	error_opts=data_err_opts,
-	denom_fill_opts={},
-	guide_opts={},
-	unc='num'
+    num=h1["Egamma"].sum("dataset"),
+    denom=h1[notdata].sum("dataset"),
+    ax=rax,
+    error_opts=data_err_opts,
+    denom_fill_opts={},
+    guide_opts={},
+    unc="num",
 )
 
 np.set_printoptions(suppress=True)
 
 
+rax.set_ylabel("Data/MC")
 
-rax.set_ylabel('Data/MC')
-
-rax.set_ylim(0,2)
+rax.set_ylim(0, 2)
 
 
 ax._get_lines.prop_cycler = ax._get_patches_for_fill.prop_cycler
-ax.autoscale(axis='x', tight=True)
-ax.set_ylim(ymin,ymax)
-ax.set_xlim(xmin,xmax)
+ax.autoscale(axis="x", tight=True)
+ax.set_ylim(ymin, ymax)
+ax.set_xlim(xmin, xmax)
 ax.set_xlabel('')
-#ax.set_yscale('log')
+ax.set_yscale('log')
 
 
-##leg = ax.legend()
+lum = plt.text(
+    1.0,
+    1.0,
+    r"53.03 fb$^{-1}$ (13 TeV)",
+    fontsize=16,
+    horizontalalignment="right",
+    verticalalignment="bottom",
+    transform=ax.transAxes,
+)
 
 
-lum = plt.text(1., 1., r"53.03 fb$^{-1}$ (13 TeV)",
-				fontsize=16,
-				horizontalalignment='right',
-				verticalalignment='bottom',
-				transform=ax.transAxes
-			   )
-
-
-
-outname = histname + "_" + file_path + ".png"
+# -- Output png
+outname = histname + "_" + file_name + ".png"
 
 plt.savefig(outname)
 plt.show()
+
