bd99fe083b03aec9adf2c16dc6d412b0634bc27f
ico1036 <jiwoong.kim@cern.ch>
2021-05-31 10:02:16 +0900

    Fit 2017

--- Coffea_WZG/Fitting/run_fit.sh
+++ Coffea_WZG/Fitting/run_fit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-#python Fit_root.py PT_1_eta_1 100
+python Fit_root.py PT_1_eta_1 10
 #python Fit_root.py PT_1_eta_2 100
 #python Fit_root.py PT_1_eta_3 50
 #python Fit_root.py PT_1_eta_4 25
@@ -19,7 +19,7 @@
 #python Fit_root.py PT_4_eta_1 50
 #python Fit_root.py PT_4_eta_2 50
 #python Fit_root.py PT_4_eta_3 10
-python Fit_root.py PT_4_eta_4 10
+#python Fit_root.py PT_4_eta_4 10
 
 
 
